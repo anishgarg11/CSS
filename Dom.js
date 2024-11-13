66eff98e53690c228dbb8df65e440b2e9e1f170e
ANISH GARG <garganish55@gmail.com>
2024-11-13 20:40:26 +0530

    commit

---
+++ Dom.js
@@ -0,0 +1,26 @@
+// let para = document.getElementsByClassName("para");
+// console.log(para);
+
+// let Query = document.querySelector(".btn"); // return 1st element on the webpage
+// console.dir(Query);
+
+// let Query1 = document.querySelectorAll(".para"); // return all "p" elements
+// console.log(Query1);
+
+
+// let div  = document.querySelector("div")
+// console.dir(div);
+
+// let h2 = document.querySelector("h2")
+// console.log(h2.innerText)
+// h2.innerText =  h2.innerText + " from Anish Garg";
+
+let divs = document.querySelectorAll(".box");
+console.log(divs);
+
+divs[0].innerText = "anish garg"
+divs[1].innerText = "Ashish garg"
+divs[2].innerText = "Avinash lnd ka topa"
+
+
+
